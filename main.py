53162e6bdeb7fbb92102027c49fca4633d50e2cc
Zoran Nikolic <zoran.nikolic1991@gmail.com>
2022-09-12 16:36:16 +0200

    get_attribute('src')

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 s = Service("C://bin/chromedriver.exe")
 driver = webdriver.Chrome(service = s)
 
-def Add_Remove():
+def Add_Remove(): #1
     driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
     time.sleep(1) # added sleep function just for better overview
     for num in range(5):
@@ -18,16 +18,19 @@ def Add_Remove():
     for num in range(4):
         driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/button[1]").click() # removing new buttons
 
-def DropDown():
+def DropDown(): #2
     driver.get("https://the-internet.herokuapp.com/dropdown")
     value = random.randint(2,3)
     driver.find_element(By.XPATH, f"//*[@id='dropdown']/option[{value}]").click() #adding random int in range of 2 and 3 inside value variable
     time.sleep(1)
 
-def Dynamic():
-    print(img)
+def Dynamic(): #3
     driver.get("https://the-internet.herokuapp.com/dynamic_content")
     for num in range(10):
+        print(driver.findElement(By.XPATH("/html/body/div[2]/div/div/div/div/div[1]/div[1]/img")).get_attribute('src'))
+        #l2 = driver.findElement(By.XPATH("/html/body/div[2]/div/div/div/div/div[1]/div[2]/img")).get_attribute('src')
+        #l3 = driver.findElement(By.XPATH("/html/body/div[2]/div/div/div/div/div[1]/div[3]/img")).get_attribute('src')
+
         driver.refresh()
 
 
