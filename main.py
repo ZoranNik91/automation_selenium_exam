105b740230bb0a8541616e16765021114216176c
Zoran Nikolic <zoran.nikolic1991@gmail.com>
2022-09-12 15:00:49 +0200

    DropDown() Random

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import random
 
 import time
 
@@ -16,7 +17,14 @@ def Add_Remove():
     for num in range(4):
         driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/button[1]").click() # removing new buttons
 
-Add_Remove() # 1. Test
+def DropDown():
+    driver.get("https://the-internet.herokuapp.com/dropdown")
+    value = random.randint(2,3)
+    driver.find_element(By.XPATH, f"//*[@id='dropdown']/option[{value}]").click() #adding random int in range of 2 and 3 inside value variable
 
-#"/html/body/div[2]/div/div/div/button[1]"
+
+Add_Remove() # 1.Test
+DropDown() # 2.Test
+
+#https://the-internet.herokuapp.com/dropdown  //*[@id="dropdown"]/option[3]
 #time.sleep(5)
