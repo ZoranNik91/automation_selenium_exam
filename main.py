c7f0893be92368961cf30a4e518984c51391d07c
Zoran Nikolic <zoran.nikolic1991@gmail.com>
2022-09-12 14:35:35 +0200

    2.updated Add_Remove

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import time
 
 s = Service("C://bin/chromedriver.exe")
 driver = webdriver.Chrome(service = s)
-driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
 
 def Add_Remove():
+    driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
     time.sleep(1) # added sleep function just for better overview
     for num in range(5):
         driver.find_element(By.XPATH,"/html/body/div[2]/div/div/button").click()  # adding new buttons //select and copy full xpath on selected button
-    time.sleep(2)
+    time.sleep(1)
     for num in range(4):
         driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/button[1]").click() # removing new buttons
 
-Add_Remove()
+Add_Remove() # 1. Test
 
 #"/html/body/div[2]/div/div/div/button[1]"
 #time.sleep(5)
