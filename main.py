4ebab4e8b00198c771ae4e90d2844efa3bba831c
Zoran Nikolic <zoran.nikolic1991@gmail.com>
2022-09-12 23:01:10 +0200

    updated Alerts()

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 import random
 from array import *
-
 import time
 
 s = Service("C://bin/chromedriver.exe")
@@ -20,6 +19,7 @@ def Add_Remove(): #1
     time.sleep(1)
     for i in range(4):
         driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/button[1]").click() # removing new buttons
+    time.sleep(1)
 
 def DropDown(): #2
     driver.get("https://the-internet.herokuapp.com/dropdown")
@@ -42,9 +42,30 @@ def Dynamic(): #3
             [print(img_list.count(x), x) for x in set(img_list)]
         driver.refresh()
 
-Add_Remove() #1.Test
-DropDown() #2.Test
-Dynamic() #3.Test
+def Alerts():
+    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+    wait = WebDriverWait(driver, 10)
 
-#for i in array:    print(array[i], end=" ")
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[1]/button").click() #JS ALert button
+    wait.until(EC.alert_is_present())
+    time.sleep(1)
+    driver.switch_to.alert.accept()
+
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[2]/button").click() #JS Confirm button
+    wait.until(EC.alert_is_present())
+    time.sleep(1)
+    driver.switch_to.alert.dismiss()
+
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[3]/button").click()  # JS Prompt button
+    wait.until(EC.alert_is_present())
+    driver.switch_to.alert.send_keys("Send Nudes")
+    time.sleep(1)
+    driver.switch_to.alert.accept()
+
+Add_Remove()#1.Test
+DropDown()  #2.Test
+Dynamic()   #3.Test
+Alerts()    #4.Test
+
+# https://the-internet.herokuapp.com/javascript_alerts
 #time.sleep(5)
