0d50674d404614b676982bed0c59385bbbf1819c
Zoran Nikolic <zoran.nikolic1991@gmail.com>
2022-09-14 01:14:23 +0200

    DOM() finished

--- main.py
+++ main.py
@@ -18,69 +18,78 @@ import base64
 s = Service("C://bin/chromedriver.exe")
 driver = webdriver.Chrome(service = s)
 
-# def Add_Remove(): #1
-#     driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
-#     time.sleep(1) # added sleep function just for better overview
-#     for i in range(5):
-#         driver.find_element(By.XPATH,"/html/body/div[2]/div/div/button").click()  # adding new buttons //select and copy full xpath on selected button
-#     time.sleep(1)
-#     for i in range(4):
-#         driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/button[1]").click() # removing new buttons
-#     time.sleep(1)
-#
-# def DropDown(): #2
-#     driver.get("https://the-internet.herokuapp.com/dropdown")
-#     value = random.randint(2,3)
-#     driver.find_element(By.XPATH, f"//*[@id='dropdown']/option[{value}]").click() #adding random int in range of 2 and 3 inside value variable
-#     time.sleep(1)
-#
-# def Dynamic(): #3
-#     driver.get("https://the-internet.herokuapp.com/dynamic_content")
-#     wait = WebDriverWait(driver, 10)
-#     img_list = []
-#     for i in range(10):
-#         l1 = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/div[1]/div[1]/img"))).get_attribute("src") # 1.image
-#         img_list.append(l1)
-#         l2 = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/div[2]/div[1]/img"))).get_attribute("src") # 2.image
-#         img_list.append(l2)
-#         l3 = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/div[3]/div[1]/img"))).get_attribute("src") # 3.image
-#         img_list.append(l3)
-#         if(i==9):
-#             [print(img_list.count(x), x) for x in set(img_list)]
-#         driver.refresh()
-#
-# def Alerts():
-#     driver.get("https://the-internet.herokuapp.com/javascript_alerts")
-#     wait = WebDriverWait(driver, 10)
-#     letters = string.ascii_letters
-#     random_string = ''.join(random.choice(letters) for i in range(10))
-#
-#     driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[1]/button").click() #JS ALert button
-#     wait.until(EC.alert_is_present())
-#     time.sleep(1)
-#     driver.switch_to.alert.accept()
-#
-#     driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[2]/button").click() #JS Confirm button
-#     wait.until(EC.alert_is_present())
-#     time.sleep(1)
-#     driver.switch_to.alert.dismiss()
-#
-#     driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[3]/button").click()  # JS Prompt button
-#     wait.until(EC.alert_is_present())
-#     driver.switch_to.alert.send_keys(random_string)
-#     time.sleep(1)
-#     driver.switch_to.alert.accept()
-#     time.sleep(1)
+def Add_Remove(): #1
+    driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
+    time.sleep(1) # added sleep function just for better overview
+    for i in range(5):
+        driver.find_element(By.XPATH,"/html/body/div[2]/div/div/button").click()  # adding new buttons //select and copy full xpath on selected button
+    time.sleep(1)
+    for i in range(4):
+        driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/button[1]").click() # removing new buttons
+    time.sleep(1)
 
-def DOM():
+def DropDown(): #2
+    driver.get("https://the-internet.herokuapp.com/dropdown")
+    value = random.randint(2,3)
+    driver.find_element(By.XPATH, f"//*[@id='dropdown']/option[{value}]").click() #adding random int in range of 2 and 3 inside value variable
+    time.sleep(1)
+
+def Dynamic():  #3
+    driver.get("https://the-internet.herokuapp.com/dynamic_content")
+    wait = WebDriverWait(driver, 10)
+    img_list = []
+    for i in range(10):
+        l1 = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/div[1]/div[1]/img"))).get_attribute("src") # 1.image
+        img_list.append(l1)
+        l2 = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/div[2]/div[1]/img"))).get_attribute("src") # 2.image
+        img_list.append(l2)
+        l3 = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/div[3]/div[1]/img"))).get_attribute("src") # 3.image
+        img_list.append(l3)
+        if(i==9):
+            [print(img_list.count(x), x) for x in set(img_list)]
+        driver.refresh()
+
+def Alerts():   #4
+    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+    wait = WebDriverWait(driver, 10)
+    letters = string.ascii_letters
+    random_string = ''.join(random.choice(letters) for i in range(10))
+
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[1]/button").click() #JS ALert button
+    wait.until(EC.alert_is_present())
+    time.sleep(1)
+    driver.switch_to.alert.accept()
+
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[2]/button").click() #JS Confirm button
+    wait.until(EC.alert_is_present())
+    time.sleep(1)
+    driver.switch_to.alert.dismiss()
+
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/ul/li[3]/button").click()  # JS Prompt button
+    wait.until(EC.alert_is_present())
+    driver.switch_to.alert.send_keys(random_string)
+    time.sleep(1)
+    driver.switch_to.alert.accept()
+    time.sleep(1)
+
+def DOM():  #5
     driver.get("https://the-internet.herokuapp.com/challenging_dom")
+
+    def TableData7():
+        result7 = []
+        table = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/div[2]/table/tbody').get_attribute("textContent").split() #get all data from table and store as array
+        for i in table:
+            if '7' in i:    #checks if string contains '7'
+                result7.append(i)
+        result7.sort() # sorted ascendingly, for desc sort set-> sort(reverse=True)
+        print(result7)
 
     def ReturnAnswer():
         canv = driver.find_element(By.XPATH, '//*[@id="content"]/script').get_attribute("textContent") #remove -> /text() to fix the error (element is not an object)
         canv = canv.split("Answer: ",1)[1]  # geting the string number after "Answer: "
         canv = int(canv.split("'")[0])      # removing everything else after number and converting string to int
         print(canv)
-        time.sleep(2)
+        time.sleep(1)
         return canv
 
     time.sleep(2)
@@ -96,12 +105,10 @@ def DOM():
     sum = qux + buz + bar
     print("SUM of all Answers: ")
     print(sum)
+    TableData7()
 
-
-# Add_Remove()#1.Test
-# DropDown()  #2.Test
-# Dynamic()   #3.Test
-# Alerts()    #4.Test
-DOM()         #5.test
-
-#comment
+Add_Remove()#1.Test
+DropDown()  #2.Test
+Dynamic()   #3.Test
+Alerts()    #4.Test
+DOM()       #5.test
